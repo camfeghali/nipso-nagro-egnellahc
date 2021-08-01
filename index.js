8e1232764b8ca9f75c3726f9b9e69ecc8fa09f3d
Camille Feghali <camfeg@gmail.com>
2021-08-01 15:38:46 +0200

    testing output STDOUT from printOrgansReceived passes

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const rebates = require("./rebates");
-let calculator = require("./calculator")(rebates);
+let calculator = require("./logger")(rebates);
 let row = { organ: "liver", cash: 10, price: 5 };
 
 calculator.outputOrgansReceived(row);
--- test/test.js
+++ test/test.js
@@ -1,6 +1,7 @@
 const rebates = require("../rebates");
 let logger = require("../logger")(rebates);
 var expect = require("expect.js");
+var assert = require("assert");
 
 describe("Logger", function () {
   describe("#calculateBonusOrgans()", function () {
@@ -8,6 +9,27 @@ describe("Logger", function () {
       let result = logger.calculateBonusOrgans("liver", 2);
       console.log("result: ", result);
       expect(result).to.eql({ heart: 0, lung: 1 });
+    });
+  });
+
+  describe("#printOrgansReceived()", function () {
+    var output, write;
+
+    write = process.stdout.write;
+
+    beforeEach(function () {
+      output = "";
+      process.stdout.write = function (str) {
+        output += str;
+      };
+    });
+
+    afterEach(function () {
+      process.stdout.write = write;
+    });
+    it("prints the organs received in the correct order and right quantities", function () {
+      logger.printOrgansReceived({ liver: 2, heart: 0, lung: 1 });
+      assert.equal(output, "heart 0, liver 2, lung 1\n");
     });
   });
 });
